1bbd24ead3d7f688e5dc1d04699a444ccfd97318
MuhammadHidayatturRahman <mhmmdyt9971@gmail.com>
2023-10-15 13:05:32 +0800

    menambah search

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username: string = "";
+  password: string = "";
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
-}
+  checkLoginAuth(){
+    //alert("username : " +this.username+" password : "+ this.password)
+    if(this.username=="dayat" && this.password=="12345"){
+      alert("Masuklah")
+      this.router.navigateByUrl('/home',{state:{userRole : "kania"}});
+    }
+    else if(this.username=="latip" && this.password=="12345") {
+      this.router.navigateByUrl('/home',{state:{userRole : "latip"}});
+    }
+    else
+      alert("SALAH COK");
+    }
+  }
+
+  
